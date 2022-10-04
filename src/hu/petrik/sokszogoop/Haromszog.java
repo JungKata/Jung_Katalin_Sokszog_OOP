41b37780a3573bf3ff9c49c2bf8e956aa7fa8ac1
JungKata <97729333+JungKata@users.noreply.github.com>
2022-10-04 14:38:13 +0200

    Háromszög osztály: - b- oldal c- oldal inicalizálása konstruktorok, getterek, setterek - kerulet terulet szerkesztheto-e függvény letrehozása

---
+++ src/hu/petrik/sokszogoop/Haromszog.java
@@ -0,0 +1,60 @@
+package hu.petrik.sokszogoop;
+
+public class Haromszog extends Sokszog {
+    private double b;
+    private  double c;
+
+    public Haromszog(double a, double b, double c) {
+        super(a);
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    public  double getKerulet()
+    {
+        return this.getA() + this.getB() + this.getC();
+    }
+
+    private boolean isSzerkesztheto()                        //get helyett is a booleannel
+    {
+        boolean szerkesztheto = true;
+        if ((this.getA() + this.getB() <= this.getC()) || (this.getA() + this.getC() <= this.getB()) || (this.getB() + this.getC() <= this.getA()))
+        {
+            szerkesztheto = false;
+        }
+        return szerkesztheto;
+    }
+
+    private double getS() {
+        return getKerulet() / 2;
+    }
+
+    public  double getTerulet()
+    {
+        return Math.sqrt(this.getS()* (this.getS() - this.getA()) *
+                (this.getS() - this.getB()) * (this.getS() - this.getC()));
+    }
+
+
+
+    @Override
+    public String toString() {
+      return "";
+    }
+}
