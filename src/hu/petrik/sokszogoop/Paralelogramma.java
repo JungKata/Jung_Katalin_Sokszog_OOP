9f3cc72f304d4d5c2da6c770cf24c985f0a80dff
JungKata <97729333+JungKata@users.noreply.github.com>
2022-10-05 20:11:55 +0200

    Paralelogramma - b oldal + alfa szög - konstruktor, getter , setter override kerulet terulet

--- src/hu/petrik/sokszogoop/Paralelogramma.java
+++ src/hu/petrik/sokszogoop/Paralelogramma.java
@@ -1,4 +1,39 @@
 package hu.petrik.sokszogoop;
 
-public class Paralelogramma {
+public class Paralelogramma extends Sokszog {
+
+    private double b;
+    private double alpha;
+
+    public Paralelogramma(double a, double b, double alpha) {
+        super(a);
+        this.b = b;
+        this.alpha = alpha;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getAlpha() {
+        return alpha;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public void setAlpha(double alpha) {
+        this.alpha = alpha;
+    }
+
+    @Override
+    public double getKerulet() {
+        return 0;
+    }
+
+    @Override
+    public double getTerulet() {
+        return 0;
+    }
 }
